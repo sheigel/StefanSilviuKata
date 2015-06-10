ad455e094bcc94bcab742756bce9c31bed4363c2
silviue <eigel.si@gmail.com>
2015-06-10 19:42:05 +0300

    final version of direct go at solution

--- spec/GameSpecs.js
+++ spec/GameSpecs.js
@@ -1,12 +1,34 @@
 /**
  * Created by silviue on 8/10/13.
  */
- describe("board parsing", function () {
+ describe("calculate nextGen", function () {
   it("returns empty grid",function  (
     ) {
     expect(nextGen([[]])).toEqual([[]]);
 })
   it("cell dies if only cell", function(){
     expect(nextGen([["*"]])).toEqual([["."]]);
+})
+
+  describe("identify neighbours", function(){
+     var neighbours = getNeighboursFor ([[".", "*", "."]],0,1);
+     expect(neighbours).toEqual([[0,0],[0,2]]);
+  });
+  describe("for 2 alive neighbours should identify cell for survival",function(){
+    var grid = [[".", "*", "."]];
+     var neighbours = getNeighboursFor (grid,0,1);
+     expect(cellSurvives(grid,neighbours)).toEqual(true);
+
   })
+
+  describe("single row",function(){
+    it("alone cell dies",function(){
+        expect(nextGen([[".", "*", "."]])).toEqual([[".",".","."]]);
+    })
+    it("cell surrounded by 2 cells survives",function(){
+        expect(nextGen([["*", "*", "*"]])).toEqual([[".","*","."]]);
+    })
+
+})
+  
 });
--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,30 @@
-function nextGen (grid) {
-   for (var i = 0; i < grid.length; i++) {
-   	for (var j = 0; j < grid[i].length; j++) {
-   		if(grid[i][j] == "*")
-   		{
-   			grid[i][j] = ".";
-   		}
-   	}
-   }
 
-   return grid;
+function getNeighboursFor (grid,i,j) {
+	var neighbouringLocations = [
+	[i-1,j-1],[i-1,j],[i-1,j+1],
+	[i,j-1],[i,j+1],
+	[i+1,j-1],[i+1,j],[i+1,j+1],
+	];
+
+	return neighbouringLocations.filter(function(loc){
+		return loc[0]>=0&&loc[0]<grid.length&&loc[1]>=0&&loc[1]<grid[0].length;
+	})
+}
+function cellSurvives (grid, neighbours) {
+	return neighbours.filter(function(loc){
+		return grid[loc[0]][loc[1]] === "*";
+	}).length===2;
+
+}
+function nextGen (grid) {
+	for (var i = 0; i < grid.length; i++) {
+		for (var j = 0; j < grid[i].length; j++) {
+			var neighbours = getNeighboursFor(grid,i,j);
+			var survives = cellSurvives(neighbours);
+			grid[i][j]=survives?"*":".";
+			
+		}
+	}
+
+	return grid;
 }
